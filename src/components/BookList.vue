45cca9ba300cca1e2081ee8daefd314adab0194b
Anthony <anthonytsang27@gmail.com>
2019-10-27 13:41:51 +0800

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,16 +2,26 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
     </ul>
-    <br><hr>
-    <book-form @addBook='appendBook'></book-form>
+    <hr />
+    <h2>Filtered Books By Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <ul class="exists">
+      <book-item v-for="book in filteredBooks" :key="book.id" :book="book"></book-item>
+    </ul>
+    <br />
+    <hr />
+    <book-form @addBook="appendBook"></book-form>
   </div>
 </template>
 
 <script>
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
+import _ from "lodash";
 
 export default {
   name: "BookList",
@@ -19,10 +29,27 @@ export default {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
+        {
+          title: "Self-Reliance",
+          author: "Ralph Waldo Emerson",
+          finishedReading: true,
+          ownership: "borrowed"
+        },
+        {
+          title: "American Gods",
+          author: "Neil Gaiman",
+          finishedReading: false,
+          ownership: "bought"
+        },
+        {
+          title: "Amusing Ourselves to Death",
+          author: "Neil Postman",
+          finishedReading: true,
+          ownership: "borrowed"
+        }
       ]
     };
   },
@@ -32,7 +59,17 @@ export default {
   },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading });
+      this.books.push({
+        title: bookData.bookTitle,
+        author: bookData.bookAuthor,
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership
+      });
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
     }
   }
 };
